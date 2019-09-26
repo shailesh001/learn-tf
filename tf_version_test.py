7ede2e3b1951380abbb99d99d0a9e16f05c80a04
shailesh <shailesh001@mac.com>
2019-09-26 21:55:47 +0100

    Added GPU check code

--- tf_version_test.py
+++ tf_version_test.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 print("Tensor Flow Version: {}".format(tf.__version__))
+
+print("GPU Available: ", tf.test.is_gpu_available())
+
+
+if tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None):
+    print('GPU')
+else:
+    print('No GPU')
