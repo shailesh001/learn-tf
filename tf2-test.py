26ebc52723a26747f625bae7ee853cdbde85432a
Shailesh Patel <shailesh001@mac.com>
2022-09-09 21:20:03 +0100

    test with CPU vs GPU

--- tf2-test.py
+++ tf2-test.py
@@ -18,6 +18,8 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+#with tf.device('/cpu:0'):
+with tf.device('/gpu:0'):
+    model.fit(x_train, y_train, epochs=10)
 
-model.evaluate(x_test, y_test)
+#model.evaluate(x_test, y_test)
